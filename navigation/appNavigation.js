93ee27274171770ad79b6bbe9ed2dea07036ec57
RushitSoni <115700646+RushitSoni@users.noreply.github.com>
2023-07-30 18:22:01 +0530

    Onboarding Screen

---
+++ navigation/appNavigation.js
@@ -0,0 +1,63 @@
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import React, { Component, useEffect, useState } from 'react'
+import { Text, View } from 'react-native'
+import HomeScreen from '../screens/HomeScreen'
+import OnBoarding from '../screens/OnBoarding'
+import { getItem } from '../utils/asyncStorage'
+
+const Stack=createNativeStackNavigator()
+
+export default function AppNavigation () {
+
+  const [showOnboarding,setShowOnboarding]=useState(null)
+
+  useEffect(()=>{
+    isOnBoarded();
+
+  },[])
+
+  const isOnBoarded =async ()=>{
+
+    let onboarded = await getItem('onboarded')
+
+    if (onboarded==1){
+
+      setShowOnboarding(false)
+
+    }
+    else{
+      setShowOnboarding(true)
+    }
+
+  }
+  if(showOnboarding==null)
+  {
+    return null
+  }
+
+  if(showOnboarding)
+  {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName='OnBoarding'>
+            <Stack.Screen name='OnBoarding' options={{headerShown:false}} component={OnBoarding}></Stack.Screen>
+            <Stack.Screen name='Home' options={{headerShown:false}} component={HomeScreen}></Stack.Screen>
+        </Stack.Navigator>
+        
+      </NavigationContainer>
+    )
+  }
+  else{
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName='Home'>
+            <Stack.Screen name='OnBoarding' options={{headerShown:false}} component={OnBoarding}></Stack.Screen>
+            <Stack.Screen name='Home' options={{headerShown:false}} component={HomeScreen}></Stack.Screen>
+        </Stack.Navigator>
+        
+      </NavigationContainer>
+    )
+  }
+  
+}
